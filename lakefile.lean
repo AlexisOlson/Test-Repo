0d9c9ade3243d7342eec4951ea983bcf6cb505a0
Alexis Olson <AlexisOlson@gmail.com>
2026-08-11 16:28:44 -0500

    pin upstream proof dependency

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,8 @@ open Lake DSL
 package stoch_to_det_lower_bound
 
 require stoch_to_det from git
-  "https://github.com/DLorell/stoch_to_det.git" @ "main"
+  "https://github.com/DLorell/stoch_to_det.git" @
+    "299c75264b07db05eab8e6d232ef88e0988f4790"
 
 @[default_target]
 lean_lib LowerBound190
